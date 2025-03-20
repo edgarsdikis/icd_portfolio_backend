ba76bdf782b0f98fa8336202a043dbe621e908d9
Edgars Dikis <edgarsdikis@startschool.org>
2025-03-21 00:30:41 +0200

    Init file

--- backend/settings/__init__.py
+++ backend/settings/__init__.py
@@ -1,8 +1,7 @@
 import os
-
-environment = os.environ.get('DJANGO_SETTINGS_MODULE', 'backend.settings.development')
-
-if environment == 'backend.settings.production':
+# Use a separate environment variable for determining which settings to load
+django_env = os.environ.get('DJANGO_ENVIRONMENT', 'development')
+if django_env == 'production':
     from .production import *
 else:
     from .development import *
